3e85e7471fa1dc95f581b626b91713b3df256d3f
solodby <solodby@gmail.com>
2019-02-15 20:51:42 +0300

    v1

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -6,8 +6,16 @@ namespace PadawansTask3
     {
         public static int Gcd(int a, int b)
         {
-            if (a <= 0 || b <= 0)
+                                          
+            if (a == 0 || b == 0)
             { throw new ArgumentException(); }
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            if (a == b)
+                return a;
+            if (b == 0)
+                return Math.Abs(a);
+            return Gcd(b, a % b);
 
 
         }
